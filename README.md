2ff5f6986af3fd1275c627841492dbbc27a57044
chflame163 <chflame@163.com>
2025-03-04 21:18:19 +0800

    update model's BaiduNetdisk link

--- README.md
+++ README.md
@@ -15,9 +15,9 @@ Install dependency packages：
 pip install -r ComfyUI_CogView4_Wrapper/requirements.txt
 ```
 
-The model will be automatically downloaded to the ```ComfyUI/models/CogView``` directory during the first run.    
+The model will be automatically downloaded to the ```ComfyUI/models/CogView/CogView4-6B``` directory during the first run.    
 
-You can also manually download the model from [huggingface.co/THUDM/CogView4-6B](https://huggingface.co/THUDM/CogView4-6B/tree/main) to the above directory.
+You can also manually download the model from [huggingface.co/THUDM/CogView4-6B](https://huggingface.co/THUDM/CogView4-6B/tree/main) or [BaiduNetdisk](https://pan.baidu.com/s/1mGbEq689Ncpc0QEDngO5Tg?pwd=id3m) to the above directory.
 
 ## Note
 ComfyUI_CgView4-Wrapper requires the installation of the diffusers dependency package in dev version, which may affect certain plugins. Please be aware of backing up the environment.
--- py/cogview4.py
+++ py/cogview4.py
@@ -29,18 +29,18 @@ class CogView4_Wrapper:
     def INPUT_TYPES(self):
         model_list =['CogView4-6B']
         dtype_list = ['bf16', 'fp32']
-        default_prompt = "A vibrant cherry red sports car sits proudly under the gleaming sun, its polished exterior smooth and flawless, casting a mirror-like reflection. The car features a low, aerodynamic body, angular headlights that gaze forward like predatory eyes, and a set of black, high-gloss racing rims that contrast starkly with the red. A subtle hint of chrome embellishes the grille and exhaust, while the tinted windows suggest a luxurious and private interior. The scene conveys a sense of speed and elegance, the car appearing as if it's about to burst into a sprint along a coastal road, with the ocean's azure waves crashing in the background.The license plate number of the car is 'CogView4'. The car sprinted along a coastal road, with the same sports car printed on the roadside billboard and large text 'GO AHEAD' written to it. The text was yellow, with thick strokes and heavy shadow lines."
+        default_prompt = "A vibrant cherry red sports car sits proudly under the gleaming sun, its polished exterior smooth and flawless, casting a mirror-like reflection. The car features a low, aerodynamic body, angular headlights that gaze forward like predatory eyes, and a set of black, high-gloss racing rims that contrast starkly with the red. A subtle hint of chrome embellishes the grille and exhaust, while the tinted windows suggest a luxurious and private interior. The scene conveys a sense of speed and elegance, the car appearing as if it's about to burst into a sprint along a coastal road, with the ocean's azure waves crashing in the background.The license plate number of the car is 'CogView4'. The car sprinted along a coastal road, with the same sports car printed on the roadside billboard and large Chinese text '遥遥领先' written to it. The text was yellow, with thick strokes and heavy shadow lines."
         return {
             "required": {
                 "model": (model_list,),
                 "dtype": (dtype_list,),
                 "prompt":("STRING", {"default":default_prompt, "multiline": True}),
                 "seed": ("INT", {"default": 0, "min": 0, "max": 1e14}),
-                "guidance_scale": ("FLOAT", {"default": 5, "min": 0.1, "max": 100, "step": 0.1}),
+                "guidance_scale": ("FLOAT", {"default": 3.5, "min": 0.1, "max": 100, "step": 0.1}),
                 "batch_size": ("INT", {"default": 1, "min": 1, "max": 100, "step": 1}),
                 "steps": ("INT", {"default": 30, "min": 1, "max": 100, "step": 1}),
-                "width": ("INT", {"default": 1024, "min": 16, "max": 4096, "step": 16}),
-                "height": ("INT", {"default": 1024, "min": 16, "max": 4096, "step": 16}),
+                "width": ("INT", {"default": 1536, "min": 16, "max": 4096, "step": 16}),
+                "height": ("INT", {"default": 832, "min": 16, "max": 4096, "step": 16}),
                 "cache_model": ("BOOLEAN", {"default": True,}),
             },
             "optional": {
